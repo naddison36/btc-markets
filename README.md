8a392b47db36984fdbf18a354544b1cd67bb8875
Nick Addison <nick@addisonbrown.com.au>
2014-10-11 00:30:55 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-btc-markets
-===========
+BTC Markets Javascript API Client
+===============
 
-BTC Markets Javascript API client
+This is a node.js wrapper for the private and public methods exposed by the [BTC Markets API](https://github.com/BTCMarkets/API).
+You will need have a registered account with [BTC Markets](https://btcmarkets.net) and generated API keys to access the private methods.
+
+Please contact support@btcmarkets.net if you are having trouble opening and account or generating an API key. 
+
+### Install
+
+`npm install btc-markets`
+
+### Examples
+
+```js
+var BTCMarkets = require('btc-markets');
+
+var client = new BTCMarkets(key, secret);
+
+var numberConverter = 100000000;    // one hundred million
+
+// get latest prices
+client.getTick("BTC", "AUD", function(err, data)
+{
+    console.log('bid ' + data.bestBid + ' ask ' + data.bestAsk + ' last price ' + data.lastPrice);
+});
+
+// get order book
+client.getOrderBook("BTC", "AUD", function(err, data)
+{
+    console.log(data.asks.length + ' asks with best ask having price ' + data.asks[0][0] +
+        ' and amount ' + data.asks[0][1]);
+});
+
+// limit buy order for of 0.01 BTC at 230 AUD
+client.createOrder("BTC", "AUD", 230 * numberConverter, 0.01 * numberConverter, 'Bid', 'Limit', 10001, function(err, data)
+{
+    console.log('id ' + data.id);
+});
+
+// market sell for 0.0001 BTC
+client.createOrder("BTC", "AUD", null, 0.0001 * numberConverter, 'Ask', 'Market', null, function(err, data)
+{
+    console.log('id ' + data.id);
+});
+
+// cancel two limit orders with id's 123456 and 987654
+client.cancelOrder([123456,987654], function(err, data)
+{
+    console.log('first order was cancelled ' + data.responses[0].success);
+});
+
+client.getAccountBalances(function(err, data)
+{
+    data.forEach(function(account)
+    {
+        console.log(account.currency + ' balance ' + account.balance / numberConverter + ' pending ' + account.pendingFunds / numberConverter);
+    });
+});
+```
---
+++ index.js
@@ -0,0 +1,220 @@
+var util = require('util'),
+    _ = require('underscore'),
+    request	= require('request'),
+    crypto = require('crypto'),
+    cheerio = require('cheerio'),
+    VError = require('verror');
+
+var BTCMarkets = function(key, secret, server, timeout)
+{
+    this.key = key;
+    this.secret = secret;
+
+    this.server = server || 'https://api.btcmarkets.net';
+
+    this.timeout = timeout || 20000;
+};
+
+BTCMarkets.prototype.privateRequest = function(path, callback, params)
+{
+    var functionName = 'BTCMarkets.privateRequest()',
+        self = this;
+
+    if(!this.key || !this.secret)
+    {
+        var error = new VError('%s must provide key and secret to make this API request.', functionName);
+        logger.error(error.stack);
+        return callback(error);
+    }
+
+    var timestamp = (new Date()).getTime();
+
+    var message;
+    if (params)
+    {
+        message = path + "\n" +
+            timestamp + "\n" +
+            JSON.stringify(params);
+    }
+    else
+    {
+        // used for /account/balance
+        message = path + "\n" + timestamp + "\n";
+    }
+
+    var signer = crypto.createHmac('sha512', new Buffer(this.secret, 'base64'));
+    var signature = signer.update(message).digest('base64');
+
+    var headers = {
+        "User-Agent": "BTC Markets Javascript API Client",
+        "apikey": this.key,
+        "timestamp": timestamp,
+        "signature": signature};
+
+    var method = 'POST';    // most API's use HTTP POST so setting as default
+
+    // The exception is /account/balance which does a HTTP GET
+    if (path === '/account/balance')
+    {
+        method = 'GET';
+        params = {};
+    }
+
+    var options = {
+        url: this.server + path,
+        method: method,
+        headers: headers,
+        timeout: this.timeout,
+        json: params };
+
+    var requestDesc = util.format('%s request to url %s with message %s',
+        options.method, options.url, message);
+
+    executeRequest(options, requestDesc, callback);
+};
+
+BTCMarkets.prototype.publicRequest = function(instrument, currency, action, callback, params)
+{
+    var functionName = 'BTCMarkets.publicRequest()';
+
+    var headers = {"User-Agent": "BTC Markets Javascript API Client"};
+
+    var path = '/market/' + instrument + '/' + currency + '/' + action;
+
+    var options = {
+        url: this.server + path,
+        method: 'GET',
+        headers: headers,
+        timeout: this.timeout,
+        json: {} };
+
+    var requestDesc = util.format('%s request to url %s',
+        options.method, options.url);
+
+    executeRequest(options, requestDesc, callback)
+};
+
+function executeRequest(options, requestDesc, callback)
+{
+    var functionName = 'BTCMarkets.executeRequest()';
+
+    request(options, function(err, response, data)
+    {
+        var error = null;   // default to no errors
+
+        if(err)
+        {
+            error = new VError(err, '%s failed %s', functionName, requestDesc);
+            error.name = err.code;
+        }
+        // if request was not able to parse json response into an object
+        else if (!_.isObject(data) )
+        {
+            // try and parse HTML body form response
+            $ = cheerio.load(data);
+
+            var responseBody = $('body').text();
+
+            if (responseBody)
+            {
+                error = new VError(err, '%s could not parse response body from %s\nResponse body: %s', functionName, requestDesc, responseBody);
+                error.name = responseBody;
+            }
+            else
+            {
+                error = new VError(err, '%s could not parse json or HTML response from %s', functionName, requestDesc);
+            }
+        }
+        else if (_.has(data, 'success') && !data.success)
+        {
+            error = new VError('%s failed %s. Error message: %s', functionName,
+                requestDesc, data.errorMessage);
+
+            error.name = data.errorMessage;
+        }
+
+        callback(error, data);
+    });
+}
+
+//
+// Public Functions
+//
+
+BTCMarkets.prototype.getTick = function getMarketSummary(instrument, currency, callback)
+{
+    this.publicRequest(instrument, currency, 'tick', callback);
+};
+
+BTCMarkets.prototype.getOrderBook = function getOrderBook(instrument, currency, callback)
+{
+    this.publicRequest(instrument, currency, 'orderbook', callback);
+};
+
+BTCMarkets.prototype.getTrades = function(instrument, currency, callback, since)
+{
+    this.publicRequest(instrument, currency, 'trades', callback, {
+        since: since}
+    );
+};
+
+//
+// Private Functions
+//
+
+BTCMarkets.prototype.createOrder = function createOrder(instrument, currency, price, volume, orderSide, ordertype, clientRequestId, callback)
+{
+    this.privateRequest('/order/create', callback, {
+        currency: currency,
+        instrument: instrument,
+        price: price,
+        volume: volume,
+        orderSide: orderSide,
+        ordertype: ordertype,
+        clientRequestId: clientRequestId}
+    );
+};
+
+BTCMarkets.prototype.cancelOrders = function cancelOrders(orderIds, callback)
+{
+    this.privateRequest('/order/cancel', callback, {
+        orderIds: orderIds}
+    );
+};
+
+BTCMarkets.prototype.getOpenOrders = function getOpenOrders(instrument, currency, limit, since, callback)
+{
+    this.privateRequest('/order/open', callback, {
+        currency: currency,
+        instrument: instrument,
+        limit: limit,
+        since: since}
+    );
+};
+
+BTCMarkets.prototype.getOrderHistory = function getOrderHistory(instrument, currency, limit, since, callback)
+{
+    this.privateRequest('/order/history', callback, {
+        currency: currency,
+        instrument: instrument,
+        limit: limit,
+        since: since}
+    );
+};
+
+BTCMarkets.prototype.getTradeHistory = function getTradeHistory(instrument, currency, limit, since, callback)
+{
+    this.privateRequest('/order/trade/history', callback, {
+        currency: currency,
+        instrument: instrument,
+        limit: limit,
+        since: since}
+    );
+};
+
+BTCMarkets.prototype.getAccountBalances = function getAccounts(callback)
+{
+    this.privateRequest('/account/balance', callback);
+};
+
+module.exports = BTCMarkets;
